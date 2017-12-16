c006ff21e087ed16828f221e02a15010221e232b
Gregory Goijaerts <crecketgaming@gmail.com>
2017-12-16 23:58:05 +0100

    Arraybuffer loading test

--- src/Helpers/FileReaderHelper.ts
+++ src/Helpers/FileReaderHelper.ts
@@ -2,13 +2,16 @@ export default async (file: File) => {
     const fileReader = new FileReader();
 
     // start loading the file as binary
-    fileReader.readAsBinaryString(file);
+    fileReader.readAsArrayBuffer(file);
 
     // wrap the filereader callback in a promise
     return new Promise((resolve, reject) => {
-
         // resolve the output onload
-        fileReader.onload = () => resolve(fileReader.result);
+        fileReader.onload = () => {
+            const arrayBuffer: any = new Uint8Array(fileReader.result);
+            const binaryString = String.fromCharCode(null, arrayBuffer);
 
+            resolve(binaryString);
+        };
     });
 };
